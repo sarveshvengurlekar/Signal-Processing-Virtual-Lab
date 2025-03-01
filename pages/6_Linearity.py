e372c86cd494bf1b41e108d7c0c3f382412f9a9e
Sarvesh Vengurlekar <141908596+sarveshvengurlekar@users.noreply.github.com>
2025-03-02 03:38:35 +0530

    Update 6_Linearity.py

--- pages/6_Linearity.py
+++ pages/6_Linearity.py
@@ -13,7 +13,7 @@ def get_base64_image(image_path):
         return base64.b64encode(img_file.read()).decode()
 
 # Path to logo
-logo_path = r"C:\Users\sarve\Downloads\Coding\Python\DSP Internship\Streamlit\Signals & System Virtual Lab\static\fcritlogo.png"
+logo_path = r"static/fcritlogo.png"
 logo_base64 = get_base64_image(logo_path)
 
 # Header with embedded Base64 logo
@@ -224,4 +224,4 @@ Thus, the system is **verified to be Linear System.**.
 
 st.header("",divider="blue")
 
-st.image(r"C:\Users\sarve\Downloads\Coding\Python\DSP Internship\Streamlit\Signals & System Virtual Lab\Media\Linearity.jpg")
+st.image(r"Media/Linearity.jpg")
