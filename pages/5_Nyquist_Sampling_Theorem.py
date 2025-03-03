f57c9f31e7f189efb9dfc55c053ea158c6b927f1
Sarvesh Vengurlekar <141908596+sarveshvengurlekar@users.noreply.github.com>
2025-03-04 01:30:42 +0530

    Update 5_Nyquist_Sampling_Theorem.py

--- pages/5_Nyquist_Sampling_Theorem.py
+++ pages/5_Nyquist_Sampling_Theorem.py
@@ -153,10 +153,6 @@ This tool is valuable for **engineers, researchers, and students** in **signal p
 st.header("",divider="blue")
 
 video_options = {
-    "4 Point DFT (DIT Type)": r"Media/4_pt_DFT_(DIT).mp4",
-    "4 Point DFT (DIF Type)": r"Media/4_pt_DFT_(DIF).mp4",
-    "8 Point DFT (DIT Type)": r"Media/8_pt_DFT_(DIT).mp4",
-    "8 Point DFT (DIF Type)": r"Media/8_pt_DFT_(DIF).mp4",
     "Nyquist Sampling Theorem" : r"Media/Nyquist (1).mp4",
 }
 
