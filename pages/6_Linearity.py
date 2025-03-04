70e4a2a3716ba895b035351d331e7bc028a30fa3
Sarvesh Vengurlekar <141908596+sarveshvengurlekar@users.noreply.github.com>
2025-03-05 03:24:16 +0530

    Update 6_Linearity.py

--- pages/6_Linearity.py
+++ pages/6_Linearity.py
@@ -117,7 +117,7 @@ A system is linear if it follows the principles of homogeneity (scaling) and sup
 ## **Condition for Linearity**
 If y1 (n) = y2 (n), then the system is linear. This means that the output produced when applying a weighted sum of inputs to the system is the same as the weighted sum of the individual outputs.
 
-Example no 01:- y (n) = n x(n)
+# Example no 01:- y (n) = n x(n)
 
 """)
 st.image(r"Media/LTI_Eg_1.png")
@@ -126,4 +126,3 @@ st.markdown("""
 # Output
 """)
 
-st.image(r"Media/Linearity.jpg")
