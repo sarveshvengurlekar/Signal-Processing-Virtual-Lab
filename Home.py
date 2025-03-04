e2011f3b0ffeb10ddb3cc98efaaea87441161ae1
Sarvesh Vengurlekar <141908596+sarveshvengurlekar@users.noreply.github.com>
2025-03-05 03:09:23 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -42,7 +42,7 @@ st.markdown(
         text-align: left;
         padding: 20px 30px;
         z-index: 1000;
-        margin-top: 148px; /* Push content below the fixed header */
+        margin-top: 100px; /* Push content below the fixed header */
         overflow: hidden;
         display: flex;
         align-items: center;
