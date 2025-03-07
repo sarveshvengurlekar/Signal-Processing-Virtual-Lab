daec235c78aa22bfe0827bd86a3b45235fd26009
Sarvesh Vengurlekar <141908596+sarveshvengurlekar@users.noreply.github.com>
2025-03-08 04:18:55 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -84,7 +84,7 @@ st.markdown(
         height: auto;
     }}
     .stApp {{
-        margin-top: 146px; /* Push content below the fixed header */
+        margin-top: 130px; /* Push content below the fixed header */
         padding-bottom: 80px; /* Avoid footer overlap */
     }}
 
