c58564b76767c9a2e9c8f10bdb974a7648ad9862
Sarvesh Vengurlekar <141908596+sarveshvengurlekar@users.noreply.github.com>
2025-03-09 04:57:21 +0530

    Update 4_Cross_Correlation.py

--- pages/4_Cross_Correlation.py
+++ pages/4_Cross_Correlation.py
@@ -5,7 +5,7 @@ import base64
 
 # Set Streamlit page configuration
 st.set_page_config(
-    page_title="Signals & Systems Virtual Lab",
+    page_title="Signal Processing Virtual Lab",
     layout="wide",
     page_icon=" "  # Placeholder for page icon
 )
