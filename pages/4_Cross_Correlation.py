a0c02f001065577b3f786d457941c3c6d6f78a72
Sarvesh Vengurlekar <141908596+sarveshvengurlekar@users.noreply.github.com>
2025-03-13 01:02:54 +0530

    Update 4_Cross_Correlation.py

--- pages/4_Cross_Correlation.py
+++ pages/4_Cross_Correlation.py
@@ -85,7 +85,7 @@ st.markdown(
         height: auto;
     }}
     .stApp {{
-        margin-top: 100px; /* Push content below the fixed header */
+        margin-top: 180px; /* Push content below the fixed header */
         padding-bottom: 80px; /* Avoid footer overlap */
     }}
 
