bc9130bfcf7b38ef74656a6cad769b337a44d978
Sarvesh Vengurlekar <141908596+sarveshvengurlekar@users.noreply.github.com>
2025-06-01 13:42:17 +0530

    Update 4_4. LTI_System.py

--- pages/4_4. LTI_System.py
+++ pages/4_4. LTI_System.py
@@ -226,6 +226,10 @@ A Low-Pass Filter with a cutoff frequency of \( 500 \, Hz \) would attenuate fre
 
 A High-Pass Filter with a cutoff frequency of \( 50 \, Hz} \) would block any frequencies below \( 50 \, Hz \), making it ideal for removing low-frequency hums or rumble.
 
+---
+
+""")
+
 local_file_path = "Media/LTI_Audio.wav"  # Update this with the correct path
 
 # Read the file in binary mode
@@ -238,11 +242,6 @@ st.download_button(
     data=wav_bytes,
     file_name="LTI_Audio.wav",
     mime="audio/wav")
-
----
-
-""")
-
 
 # Function to convert audio to base64 for browser playback
 def get_audio_base64(audio, sample_rate):
